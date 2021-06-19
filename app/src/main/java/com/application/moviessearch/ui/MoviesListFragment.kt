61353844fba241bdf00acb86d283a4f2cb353e83
Mohamed Ismail <mohamedismail77@yahoo.com>
2021-06-19 19:34:26 +0200

    adding loading state adapter and retry functionality

--- app/src/main/java/com/application/moviessearch/ui/MoviesListFragment.kt
+++ app/src/main/java/com/application/moviessearch/ui/MoviesListFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-    import androidx.fragment.app.viewModels
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import com.application.moviessearch.R
 import com.application.moviessearch.databinding.FragmentMoviesListBinding
+import com.application.moviestest.MoviesLoadStateAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -21,14 +25,6 @@ class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
     private var _binding: FragmentMoviesListBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentMoviesListBinding.bind(view)
@@ -38,8 +34,39 @@ class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
         binding.apply {
             recyclerView.setHasFixedSize(true)
             recyclerView.itemAnimator = null
-            recyclerView.adapter = adapter
+            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = MoviesLoadStateAdapter { adapter.retry() },
+                footer = MoviesLoadStateAdapter { adapter.retry() },
+            )
         }
+
+        adapter.addLoadStateListener { loadState ->
+            val isListEmpty = loadState.refresh is LoadState.NotLoading && adapter.itemCount == 0
+            showEmptyList(isListEmpty)
+
+            // Only show the list if refresh succeeds.
+            binding.recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+            // Show loading spinner during initial load or refresh.
+            binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+            // Show the retry state if initial load or refresh fails.
+            binding.retryButton.isVisible = loadState.source.refresh is LoadState.Error
+
+            val errorState = loadState.source.append as? LoadState.Error
+                ?: loadState.source.prepend as? LoadState.Error
+                ?: loadState.append as? LoadState.Error
+                ?: loadState.prepend as? LoadState.Error
+            errorState?.let {
+                Toast.makeText(
+                    requireActivity(),
+                    "\uD83D\uDE28 Wooops ${it.error}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+        }
+
+        binding.retryButton.setOnClickListener { adapter.retry() }
+
 
         lifecycleScope.launch {
             moviesListViewModel.getMoviesList().collect {
@@ -47,6 +74,16 @@ class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
             }
         }
 
+    }
+
+    private fun showEmptyList(show: Boolean) {
+        if (show) {
+            binding.emptyList.visibility = View.VISIBLE
+            binding.recyclerView.visibility = View.GONE
+        } else {
+            binding.emptyList.visibility = View.GONE
+            binding.recyclerView.visibility = View.VISIBLE
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/application/moviessearch/ui/MoviesLoadStateAdapter.kt
@@ -0,0 +1,15 @@
+package com.application.moviestest
+
+import android.view.ViewGroup
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+
+class MoviesLoadStateAdapter(private val retry: () -> Unit) : LoadStateAdapter<MoviesLoadStateViewHolder>() {
+    override fun onBindViewHolder(holder: MoviesLoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): MoviesLoadStateViewHolder {
+        return MoviesLoadStateViewHolder.create(parent, retry)
+    }
+}
